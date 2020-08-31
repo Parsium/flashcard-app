d262c4b9b932850960191ebdcbebb87c70602c9b
James Parslow <james@parslows.com>
2020-08-31 15:15:13 +0100

    Fixed error with validating answer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,13 @@ import AnswerBox from "./AnswerBox";
 const App = ({deck}) => {
     const STATUSES = {question: "question", hint: "hint", correct: "correct", incorrect: "incorrect"}; 
 
-    const [state, setState] = useState({...deck[0], status: STATUSES.question});
+    const [state, setState] = useState({queue: deck, status: STATUSES.question});
 
     function validateAnswer(text) {
-        var answer = JSON.stringify(text.toLowerCase()).slice(1,-1);
+        var userAnswer = JSON.stringify(text.toLowerCase()).slice(1,-1);
+        var realAnswer = state.queue[0].answer;
 
-        if (state.answer == String(answer)) {
+        if (realAnswer === String(userAnswer)) {
             setState(prevState => { return {...prevState, status: STATUSES.correct} });
         } else {
             setState(prevState => { return {...prevState, status: STATUSES.incorrect} });
@@ -25,51 +26,72 @@ const App = ({deck}) => {
             case STATUSES.hint:
                 setState(prevState => { return {...prevState, status: STATUSES.question} });
                 break;
+            default:
+                break;
         }
     }
 
     function skipCard() {
-        return;
+        let copiedQueue = Object.assign([], state.queue);
+        let skipped = copiedQueue.shift();
+        copiedQueue.push(skipped);
+        setState(
+            {queue: copiedQueue, status: STATUSES.question}
+        );
     }
 
     function nextCard() {
-        return;
+        let copiedQueue = Object.assign([], state.queue);
+        setState(
+            {queue: copiedQueue.slice(1), status: STATUSES.question}
+        );
     }
 
-    var message = state.question;
-    switch (state.status) {
-        case STATUSES.hint:
-            message = state.answer;
-            break;
-        case STATUSES.correct:
-            message = "Correct"
-            break;
-        case STATUSES.incorrect:
-            message = "Incorrect";
-            break;
-    }
+    let cardPreview = () => {
+        let card = state.queue[0];
+
+        var message = card.question;
+        switch (state.status) {
+            case STATUSES.hint:
+                message = card.answer;
+                break;
+            case STATUSES.correct:
+                message = "Correct"
+                break;
+            case STATUSES.incorrect:
+                message = "Incorrect";
+                break;
+            default:
+                break;
+        }
+
+        return (
+            <CardPreview
+                id={card.id}
+                message={message}
+                status={state.status}
+                onClick={() => toggleHint()}
+            ></CardPreview>
+        );
+    };
+
+    let buttons = () => {
+        if (state.status == STATUSES.correct || state == STATUSES.incorrect) {
+            return <button className="control-group" type="submit" onClick={() => nextCard()}>Next card</button>
+        }
+        return <button className="control-group" type="submit" onClick={() => skipCard()}>Skip card</button>
+    };
 
     return (
         <div id="page-container">
             <h1>Deck</h1>
-            <CardPreview
-                id={state.id}
-                message={ message }
-                status={state.status}
-                onClick={() => toggleHint()}
-            >
-            </CardPreview>
+            {cardPreview()}
             <AnswerBox
                 onClick={(text) => {
                     validateAnswer(text);
                 }}>
             </AnswerBox>
-            <button
-                className="control-group"
-                type="submit"
-                onClick={() => {}}>
-                    Next
-            </button>
+            {buttons()}
         </div>
     );
 };
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,13 +6,6 @@ import userEvent from '@testing-library/user-event';
 import App from '../components/app';
 import CardPreview from '../components/CardPreview';
 
-const TEST_DECK = [
-    {id: 0, question: "What colour are the four stars on the flag of New Zealand?", answer:"red"},
-    {id: 1, question: "How many states make up the United States of America?", answer:"50"},
-    {id: 2, question: "By what name is the TV adventurer Edward Michael Grylls more commonly known?", answer: "bear grylls"},
-    {id: 3, question: "From which language is the word ‘ketchup’ derived?", answer:"chinese"}
-  ]; 
-
 describe("App", () => {
     test("clicking on card correctly toggles between displaying question and answer", () => {
         const deck = [
